182590152bde30725442a80c9752c9e4e041c38e
Alexeykoh <Alexeykoh@icloud.com>
2024-01-11 15:14:24 +0300

    feat: solve getIntervalArray and getIntervalArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -17,11 +17,11 @@
  * @example
  *    getIntervalArray(1, 5)  => [ 1, 2, 3, 4, 5 ]
  *    getIntervalArray(-2, 2)  => [ -2, -1, 0, 1, 2 ]
- *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
+ *    getIntervalArray(0, 100)` => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return Array.from({ length: end - start + 1 }, (v, i) => start + i);
 }
 
 /**
@@ -37,8 +37,14 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const arrays = [arr1, arr2].sort((a, b) => b.length - a.length);
+  const res = arrays[0].map((a, ind) => {
+    console.log(a);
+    return a + (arrays[1][ind] || 0);
+  }, 0);
+  console.log('res', res);
+  return res;
 }
 
 /**
